660ba0a4312781036c09e33136d82a1d24bc3cef
liqidan <2360355443@qq.com>
2016-07-06 16:34:24 +0800

    commit

--- jsdoc.js
+++ jsdoc.js
@@ -2,7 +2,7 @@ var exec = require('child_process').exec;
 
 exec("jsdoc -c jsdoc-config.json -r", function(err,stdout,stderr) {
     if(err) {
-        console.log('jsdoc 出错了:'+stderr);
+        console.log('jsdoc 出错了 :'+stderr);
     } else {
         console.log('jsdoc 创建完成了 !');
     }
