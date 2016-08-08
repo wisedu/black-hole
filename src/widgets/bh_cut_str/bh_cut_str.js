0d00370d06948f5f80c25f91b6f4947c1a79d001
xujiabin0 <xujiabin0114@163.com>
2016-08-08 11:06:59 +0800

    from xujiabin about bhMenu bhCutStr

--- src/widgets/bh_cut_str/bh_cut_str.js
+++ src/widgets/bh_cut_str/bh_cut_str.js
@@ -1,214 +1,286 @@
+/**
+ * @fileOverview 字符截断组件
+ * @example
+$.bhCutStr({
+    dom: { //可选，当要显示文本的dom结构已经存在，选用此方法
+        selector: '', //必选，截断文本所在容器的jquery选择器
+        line: 0, //必选，指定显示的文本行数
+        content: '' //可选，传入的文本内容（若文本已经加载到dom里了，则此项可不加）
+    },
+    text: { //可选，纯粹的对字符串做截断处理,使用该方式显示更多的处理自能自己处理
+        content: '', //必填，传入的文本内容
+        number: 0 //要截断的字符个数
+    },
+    moreBtn: {
+        content: '', //可选，更多按钮的文字
+        url: '', //可选，超链接所指向的地址
+        isOpenNewPage: true //可选，当url存在时，该选项才能生效，默认是打开新页面
+    }
+});
+ */
 (function($) {
-	'use strict';
+  'use strict';
+  /**
+   * 这里是一个自运行的单例模式。
+   * @module bhCutStr
+   */
+  $.bhCutStr = function(data) {
+    /**
+     * @memberof module:bhCutStr
+     * @description 内置默认值
+     * @prop {object}  data
+     * 
+     * @prop {object}  data.dom - 可选，当要显示文本的dom结构已经存在，选用此方法
+     * @prop {$}       data.dom.selector - 必选，截断文本所在容器的jquery选择器
+     * @prop {number}  data.dom.line - 必选，指定显示的文本行数
+     * @prop {string}  data.dom.content - 可选，传入的文本内容（若文本已经加载到dom里了，则此项可不加）
+     * 
+     * @prop {object}  data.text - 可选，纯粹的对字符串做截断处理,使用该方式显示更多的处理
+     * @prop {number}  data.text.line - 必选，要截断的字符个数
+     * @prop {string}  data.text.content - 必填，传入的文本内容
+     * 
+     * @prop {object}  data.moreBtn - 可选，设置更多按钮
+     * @prop {string}  data.moreBtn.content - 可选，更多按钮的文字
+     * @prop {string}  data.moreBtn.url - 可选，超链接所指向的地址
+     * @prop {boolean} data.moreBtn.isOpenNewPage - 可选，当url存在时，该选项才能生效，默认是打开新页面
+     * 
+     */
+    var bhCutStrDefaults = {
+      dom: { //可选，当要显示文本的dom结构已经存在，选用此方法
+        selector: '', //必选，截断文本所在容器的jquery选择器
+        line: 0, //必选，指定显示的文本行数
+        content: '' //可选，传入的文本内容（若文本已经加载到dom里了，则此项可不加）
+      },
+      text: { //可选，纯粹的对字符串做截断处理,使用该方式显示更多的处理自能自己处理
+        content: '', //必填，传入的文本内容
+        number: 0 //要截断的字符个数
+      },
+      moreBtn: {
+        content: '', //可选，更多按钮的文字
+        url: '', //可选，超链接所指向的地址
+        isOpenNewPage: true //可选，当url存在时，该选项才能生效，默认是打开新页面
+      }
+    };
+    //将{}, bhCutStrDefaults, data进行合并，然后将合并结果返回给options
+    var options = $.extend({}, bhCutStrDefaults, data);
 
-	$.bhCutStr = function(data) {
-		var bhCutStrDefaults = {
-			dom: {//可选，当要显示文本的dom结构已经存在，选用此方法
-				selector: '', //必选，截断文本所在容器的jquery选择器
-				line: 0, //必选，指定显示的文本行数
-				content: '' //可选，传入的文本内容（若文本已经加载到dom里了，则此项可不加）
-			},
-			text: {//可选，纯粹的对字符串做截断处理,使用该方式显示更多的处理自能自己处理
-				content: '',//必填，传入的文本内容
-				number: 0 //要截断的字符个数
-			},
-			moreBtn: {
-				content: '', //可选，更多按钮的文字
-				url: '', //可选，超链接所指向的地址
-				isOpenNewPage: true //可选，当url存在时，该选项才能生效，默认是打开新页面
-			}
-		};
-		var options = $.extend({}, bhCutStrDefaults, data);
+    return init(options);
+    /**
+     * 初始化字符串截断函数
+     * @param  options
+     * @return string
+     */
+    function init(options) {
+      var $objArr = options.dom ? options.dom.selector : "";
+      if ($objArr.length > 0) {
+        $objArr.each(function() {
+          /*生成随机字符串*/
+          var guid = BH_UTILS.NewGuid();
+          var $obj = $(this);
+          //在该dom下添加一个零时dom来获取这个块的font-size
+          $obj.append(
+            '<div id="' + guid + '">' +
+            '<div id="chinese-' + guid + '" style="display: none;position: absolute;">是</div>' +
+            '<div id="lower-english-' + guid + '" style="display: none;position: absolute;">w</div>' +
+            '</div>');
+          var tempObj = $obj.find('#' + guid);
+          //中文字符和大写的英文的大小
+          options.chineseFontSize = tempObj.find('#chinese-' + guid).width();
+          //小写的英文和数字的大小
+          options.lowerEnFontSize = tempObj.find('#lower-english-' + guid).width();
+          tempObj.remove();
 
-		return init(options);
+          //获取文本所在dom的宽度
+          var objWidth = $obj.width();
+          var objText = options.dom.content ? $.trim(options.dom.content) : $.trim($obj.text()); //获得字符串内容
+          var textLen = objText.length; //实际字符个数
 
-		function init(options) {
-			var $objArr = options.dom ? options.dom.selector : "";
-			if($objArr.length > 0){
-				$objArr.each(function() {
-					var guid = BH_UTILS.NewGuid();
-					var $obj = $(this);
-					//在该dom下添加一个零时dom来获取这个块的font-size
-					$obj.append(
-							'<div id="'+guid+'">' +
-								'<div id="chinese-'+guid+'" style="display: none;position: absolute;">是</div>' +
-								'<div id="lower-english-'+guid+'" style="display: none;position: absolute;">w</div>' +
-							'</div>');
-					var tempObj = $obj.find('#'+guid);
-					//中文字符和大写的英文的大小
-					options.chineseFontSize = tempObj.find('#chinese-'+guid).width();
-					//小写的英文和数字的大小
-					options.lowerEnFontSize = tempObj.find('#lower-english-'+guid).width();
-					tempObj.remove();
+          //按所有文本都是中文字符计算出的文字个数
+          var tempComputeTextLen = parseInt(objWidth * options.dom.line / options.chineseFontSize, 10);
+          //当内容的字符数比计算出的字符数少，则不做任何处理
+          if (textLen <= tempComputeTextLen) {
+            return;
+          }
 
-					//获取文本所在dom的宽度
-					var objWidth = $obj.width();
-					var objText = options.dom.content ? $.trim(options.dom.content) : $.trim($obj.text()); //获得字符串内容
-					var textLen = objText.length; //实际字符个数
+          var url = 'javascript:void(0);';
+          var target = 'target="blank"';
+          var moreStr = '';
+          var moreText = '';
+          if (options.moreBtn) {
+            url = !options.moreBtn.url || options.moreBtn.url.length === 0 ? 'javascript:void(0);' : options.moreBtn.url;
+            target = !options.moreBtn.isOpenNewPage ? 'target="blank"' : '';
+            moreText = options.moreBtn.content ? options.moreBtn.content : '';
+            moreStr = '<a href="' + url + '" class="bh-cut-str-more" bh-cut-str-role="bhCutStr" ' + target + ' data-guid="' + guid + '" data-full-str="' + objText + '">' + moreText + '</a>';
+          }
 
-					//按所有文本都是中文字符计算出的文字个数
-					var tempComputeTextLen = parseInt(objWidth * options.dom.line / options.chineseFontSize,10);
-					//当内容的字符数比计算出的字符数少，则不做任何处理
-					if(textLen <= tempComputeTextLen){
-						return;
-					}
+          //更多按钮的宽度
+          var moreTextWidth = moreText ? getTextWidth(moreText) : 0;
+          //省略号的宽度，按一个中文字符的宽度算
+          var ellipsisWidth = options.chineseFontSize;
+          //去掉更多按钮和省略号的剩余宽度和一个字符的偏差宽度
+          var canUseWidth = objWidth * options.dom.line - moreTextWidth - ellipsisWidth - options.chineseFontSize;
+          var computeTextLen = parseInt(canUseWidth / options.chineseFontSize, 10);
+          var computeText = getCutText(canUseWidth, computeTextLen, objText);
 
-					var url = 'javascript:void(0);';
-					var target = 'target="blank"';
-					var moreStr = '';
-					var moreText = '';
-					if(options.moreBtn){
-						url = !options.moreBtn.url || options.moreBtn.url.length === 0 ? 'javascript:void(0);' : options.moreBtn.url;
-						target = !options.moreBtn.isOpenNewPage ? 'target="blank"' : '';
-						moreText = options.moreBtn.content ? options.moreBtn.content : '';
-						moreStr = '<a href="' + url + '" class="bh-cut-str-more" bh-cut-str-role="bhCutStr" '+target+' data-guid="'+guid+'" data-full-str="'+objText+'">' + moreText + '</a>';
-					}
+          $obj.html(computeText + "..." + moreStr);
 
-					//更多按钮的宽度
-					var moreTextWidth = moreText ? getTextWidth(moreText) : 0;
-					//省略号的宽度，按一个中文字符的宽度算
-					var ellipsisWidth = options.chineseFontSize;
-					//去掉更多按钮和省略号的剩余宽度和一个字符的偏差宽度
-					var canUseWidth = objWidth * options.dom.line - moreTextWidth - ellipsisWidth - options.chineseFontSize;
-					var computeTextLen = parseInt(canUseWidth / options.chineseFontSize,10);
-					var computeText = getCutText(canUseWidth, computeTextLen, objText);
+          if (moreStr) {
+            moreTextEvent($obj.find('a[bh-cut-str-role="bhCutStr"]'));
+          }
+        });
+      } else {
+        var text = $.trim(options.text.content);
+        var cutText = text;
+        if (text.length > options.text.number) {
+          cutText = text.substring(0, options.text.number) + '...';
+        }
 
-					$obj.html(computeText + "..." + moreStr);
+        var url = 'javascript:void(0);';
+        var target = 'target="blank"';
+        var moreStr = '';
+        var moreText = '';
+        if (options.moreBtn) {
+          url = !options.moreBtn.url || options.moreBtn.url.length === 0 ? 'javascript:void(0);' : options.moreBtn.url;
+          target = !options.moreBtn.isOpenNewPage ? 'target="blank"' : '';
+          moreText = options.moreBtn.content ? options.moreBtn.content : '';
+          moreStr = '<a href="' + url + '" class="bh-cut-str-more" bh-cut-str-role="bhCutStr" ' + target + ' data-full-str="' + text + '">' + moreText + '</a>';
+        }
 
-					if(moreStr){
-						moreTextEvent($obj.find('a[bh-cut-str-role="bhCutStr"]'));
-					}
-				});
-			}else{
-				var text = $.trim(options.text.content);
-				var cutText = text;
-				if(text.length > options.text.number){
-					cutText = text.substring(0, options.text.number) + '...';
-				}
+        cutText += moreStr;
+        return cutText;
+      }
+    }
+    /**
+     * [getCutText 获取截断文本]
+     * @param  {number} width   可以使用的宽度
+     * @param  {number} textLen 计算后文本的字符数
+     * @param  {string} text    字符串内容
+     * @return {string}
+     */
+    function getCutText(width, textLen, text) {
+      //按传入的长度截取字符
+      var cutText = text.substring(0, textLen);
+      //截取后剩余的字符
+      var surplusText = text.substring(textLen, text.length);
+      //获取该字符串的宽度
+      var textWidth = getTextWidth(cutText);
+      //当字符的宽度小于理论宽度，计算偏差字符
+      var diffText = '';
+      if (textWidth < width) {
+        var diffWidth = width - textWidth;
+        //当偏差的宽度大于1个中文字符时，继续截断
+        if (diffWidth > options.chineseFontSize) {
+          var diffTextLen = parseInt(diffWidth / options.chineseFontSize, 10);
+          diffText = getCutText(diffWidth, diffTextLen, surplusText);
+        }
+      }
+      return cutText + diffText;
+    }
+    /**
+     * [getTextWidth 获取文本宽度]
+     * @param  {type} text 目标文本
+     * @return {type}
+     */
+    function getTextWidth(text) {
+      //匹配中文
+      var chinese = text.match(/[^\x00-\xff]/g);
+      var chineseLen = chinese ? chinese.length : 0;
+      //大写字母和@
+      var capitalEn = text.match(/[A-Z@]/g);
+      var capitalEnLen = capitalEn ? capitalEn.length : 0;
+      var lowerEnLen = text.length - chineseLen - capitalEnLen;
+      //大写字母按中文字符的宽度算
+      var width = (chineseLen + capitalEnLen) * options.chineseFontSize + lowerEnLen * options.lowerEnFontSize;
+      return width;
+    }
+    /**
+     * [moreTextEvent 查看更多文本]
+     * @param  {$} $moreStr jquery选择器
+     *  
+     */
+    function moreTextEvent($moreStr) {
+      $moreStr.on('mouseover', function() {
+        var text = $moreStr.attr('data-full-str');
+        var guid = $moreStr.attr('data-guid');
 
-				var url = 'javascript:void(0);';
-				var target = 'target="blank"';
-				var moreStr = '';
-				var moreText = '';
-				if(options.moreBtn){
-					url = !options.moreBtn.url || options.moreBtn.url.length === 0 ? 'javascript:void(0);' : options.moreBtn.url;
-					target = !options.moreBtn.isOpenNewPage ? 'target="blank"' : '';
-					moreText = options.moreBtn.content ? options.moreBtn.content : '';
-					moreStr = '<a href="' + url + '" class="bh-cut-str-more" bh-cut-str-role="bhCutStr" '+target+' data-full-str="'+text+'">' + moreText + '</a>';
-				}
+        var $textContainer = $moreStr.parent();
+        var containerWidth = $textContainer.width();
+        setAndShowPopBoxPosition($moreStr, containerWidth, text, guid);
+      });
+      $moreStr.on('mouseout', function() {
+        var guid = $moreStr.attr('data-guid');
+        var $box = $('#' + guid);
+        moreBoxToHide($box);
+      });
+    }
 
-				cutText += moreStr;
-				return cutText;
-			}
-		}
+    function moreBoxEvent($moreBox) {
+      $moreBox.on('mouseover', function() {
+        $moreBox.data('flag', 'show');
+      });
+      $moreBox.on('mouseout', function() {
+        moreBoxToHide($moreBox);
+      });
+    }
 
-		function getCutText(width, textLen, text){
-			//按传入的长度截取字符
-			var cutText = text.substring(0, textLen);
-			//截取后剩余的字符
-			var surplusText = text.substring(textLen, text.length);
-			//获取该字符串的宽度
-			var textWidth = getTextWidth(cutText);
-			//当字符的宽度小于理论宽度，计算偏差字符
-			var diffText = '';
-			if(textWidth < width){
-				var diffWidth = width - textWidth;
-				//当偏差的宽度大于1个中文字符时，继续截断
-				if(diffWidth > options.chineseFontSize){
-					var diffTextLen = parseInt(diffWidth / options.chineseFontSize, 10);
-					diffText = getCutText(diffWidth, diffTextLen, surplusText);
-				}
-			}
-			return cutText + diffText;
-		}
+    function moreBoxToHide($box) {
+      $box.data('flag', 'hide');
+      setTimeout(function() {
+        if ($box.data('flag') === 'hide') {
+          $box.removeClass('bh-active');
+          setTimeout(function() {
+            $box.remove();
+          }, 250);
+        }
+      }, 150);
+    }
+    /**
+     * [setAndShowPopBoxPosition 设置与展示气泡框的位置]
+     * @param {$} $element 
+     * @param {number} boxWidth 
+     * @param {string} content  
+     *      
+     */
+    function setAndShowPopBoxPosition($element, boxWidth, content, id) {
+      var elementPosition = BH_UTILS.getElementPosition($element);
+      var windowHeight = window.innerHeight;
+      var halfWindowHeight = parseInt(windowHeight / 2, 10);
+      var windowScrollTop = window.scrollY;
+      var $content = '<div data-flag="content">' + content + '</div>';
+      var frameHtml = '<div id="' + id + '" style="width: ' + boxWidth + 'px;" class="bh-cutStr-popBox bh-card bh-card-lv2 bh-animate-transform-base bh-animate-scale">' + $content + '</div>';
+      var $frameHtml = $(frameHtml);
+      $('body').append($frameHtml);
 
-		function getTextWidth(text){
-			//匹配中文
-			var chinese = text.match(/[^\x00-\xff]/g);
-			var chineseLen = chinese ? chinese.length : 0;
-			//大写字母和@
-			var capitalEn = text.match(/[A-Z@]/g);
-			var capitalEnLen = capitalEn ? capitalEn.length : 0;
-			var lowerEnLen = text.length - chineseLen - capitalEnLen;
-			//大写字母按中文字符的宽度算
-			var width = (chineseLen + capitalEnLen) * options.chineseFontSize + lowerEnLen * options.lowerEnFontSize;
-			return width;
-		}
+      //默认展开的起点是右上角
+      var boxStyle = {
+        left: elementPosition.right - boxWidth + 'px',
+        top: elementPosition.bottom + 'px'
+      };
+      var boxOriginClass = 'bh-animate-origin-TR';
+      var contentHeight = $frameHtml.outerHeight();
+      var maxHeight = 0; //更多展示框的最大高度
+      //查看当前位置高度，辨别是上部还是下部的可用空间较多
+      var canShowHeight = 0;
+      var diff = 8; //留出弹框距离页面的边距
+      if (elementPosition.top - windowScrollTop > halfWindowHeight) {
+        //当上部有更多展示空间的处理
+        canShowHeight = elementPosition.top - windowScrollTop - $('header[bh-header-role="bhHeader"]').outerHeight() - diff;
+        boxOriginClass = 'bh-animate-origin-BR';
+        maxHeight = canShowHeight > contentHeight ? contentHeight : canShowHeight;
+        boxStyle.top = elementPosition.top - maxHeight + 'px';
+      } else {
+        //当下部有更多展示空间的处理
+        canShowHeight = windowHeight + windowScrollTop - elementPosition.bottom - $('footer[class="bh-footer"]').outerHeight() - diff;
+        maxHeight = canShowHeight > contentHeight ? contentHeight : canShowHeight;
+      }
 
-		function moreTextEvent($moreStr){
-			$moreStr.on('mouseover', function(){
-				var text = $moreStr.attr('data-full-str');
-				var guid = $moreStr.attr('data-guid');
+      boxStyle['max-height'] = maxHeight + 'px';
 
-				var $textContainer = $moreStr.parent();
-				var containerWidth = $textContainer.width();
-				setAndShowPopBoxPosition($moreStr, containerWidth, text, guid);
-			});
-			$moreStr.on('mouseout', function(){
-				var guid = $moreStr.attr('data-guid');
-				var $box = $('#'+guid);
-				moreBoxToHide($box);
-			});
-		}
-
-		function moreBoxEvent($moreBox){
-			$moreBox.on('mouseover', function(){
-				$moreBox.data('flag', 'show');
-			});
-			$moreBox.on('mouseout', function(){
-				moreBoxToHide($moreBox);
-			});
-		}
-
-		function moreBoxToHide($box){
-			$box.data('flag', 'hide');
-			setTimeout(function(){
-				if($box.data('flag') === 'hide'){
-					$box.removeClass('bh-active');
-					setTimeout(function(){
-						$box.remove();
-					}, 250);
-				}
-			}, 150);
-		}
-
-		function setAndShowPopBoxPosition($element, boxWidth, content, id){
-			var elementPosition = BH_UTILS.getElementPosition($element);
-			var windowHeight = window.innerHeight;
-			var halfWindowHeight = parseInt(windowHeight/2, 10);
-			var windowScrollTop = window.scrollY;
-			var $content = '<div data-flag="content">'+content+'</div>';
-			var frameHtml = '<div id="'+id+'" style="width: '+boxWidth+'px;" class="bh-cutStr-popBox bh-card bh-card-lv2 bh-animate-transform-base bh-animate-scale">'+$content+'</div>';
-			var $frameHtml = $(frameHtml);
-			$('body').append($frameHtml);
-
-			//默认展开的起点是右上角
-			var boxStyle = {left: elementPosition.right - boxWidth +'px', top: elementPosition.bottom+'px'};
-			var boxOriginClass = 'bh-animate-origin-TR';
-			var contentHeight = $frameHtml.outerHeight();
-			var maxHeight = 0; //更多展示框的最大高度
-			//查看当前位置高度，辨别是上部还是下部的可用空间较多
-			var canShowHeight = 0;
-			var diff = 8; //留出弹框距离页面的边距
-			if(elementPosition.top - windowScrollTop > halfWindowHeight){
-				//当上部有更多展示空间的处理
-				canShowHeight = elementPosition.top - windowScrollTop - $('header[bh-header-role="bhHeader"]').outerHeight() - diff;
-				boxOriginClass = 'bh-animate-origin-BR';
-				maxHeight = canShowHeight > contentHeight ? contentHeight : canShowHeight;
-				boxStyle.top = elementPosition.top - maxHeight+'px';
-			}else{
-				//当下部有更多展示空间的处理
-				canShowHeight = windowHeight + windowScrollTop - elementPosition.bottom - $('footer[class="bh-footer"]').outerHeight() - diff;
-				maxHeight = canShowHeight > contentHeight ? contentHeight : canShowHeight;
-			}
-
-			boxStyle['max-height'] = maxHeight+'px';
-
-			$frameHtml.css(boxStyle).addClass(boxOriginClass);
-			setTimeout(function(){
-				$frameHtml.addClass('bh-active');
-			},10);
-			moreBoxEvent($frameHtml);
-		}
-	}
+      $frameHtml.css(boxStyle).addClass(boxOriginClass);
+      setTimeout(function() {
+        $frameHtml.addClass('bh-active');
+      }, 10);
+      moreBoxEvent($frameHtml);
+    }
+  }
 })(jQuery);
--- src/widgets/bh_menu/bh_menu.js
+++ src/widgets/bh_menu/bh_menu.js
@@ -1,17 +1,37 @@
 /**
- * 类似于纵向tab页签
+ * @fileOverview 纵向菜单导航
+ * @example
+var _source = [
+    {
+        id:"1",
+        title:"摘要内容",
+        url:"scenes_course_info"
+    },
+    {
+        id:"2",
+        title:"基本信息",
+        url:"#"
+    }
+];
+$(".sc-leftSide-courseMenu").bhMenu({
+    source:_source,//必填, 菜单项的数据 id,title 如果是mode="link"需要传url
+    activeIndex:0, //可选, 选中第几项, 默认为0
+    contentContainer:$("body"), //可选, 内容所在的容器
+    mode:"link" //可选, 支持页面跳转还是tab页切换 支持两个值link/tab 默认值tab
+});
  */
-(function ($) {
+(function($) {
     /**
      * 定义一个插件
      */
     var Plugin;
 
-    var activeId,source,$linkContainer,$contentContainer,mode;
+    var activeId, source, $linkContainer, $contentContainer, mode;
     /**
      * 这里是一个自运行的单例模式。
+     * @module bhMenu
      */
-    Plugin = (function () {
+    Plugin = (function() {
 
         /**
          * 插件实例化部分，初始化时调用的代码可以放这里
@@ -23,9 +43,21 @@
             this.$element = $(element);
             init(this.settings, this.$element);
         }
+        /**
+         * enable一项
+         * @method enableItem
+         * @example
+        $control.bhMenu("enableItem",1);
+         */
         Plugin.prototype.enableItem = function(index) {
             enableByIndex(index);
         };
+        /**
+         * disable一项
+         * @method disableItem
+         * @example
+        $control.bhMenu("disableItem",1);
+         */
         Plugin.prototype.disableItem = function(index) {
             disableByIndex(index);
         };
@@ -33,7 +65,7 @@
 
     })();
 
-    function init(options, dom){
+    function init(options, dom) {
         //初始化头部
         source = options.source;
         $linkContainer = dom;
@@ -45,14 +77,14 @@
         addEventListener(options);
     }
 
-    function initActive(_index){
+    function initActive(_index) {
         var menuId = getIdByIndex(_index);
         setActive(menuId);
     }
 
-    function setActive(menuId){
-        if(menuId == null) return;
-        var $menuItem = $linkContainer.find(".bh-menu-link-item[menuId='"+menuId+"']");
+    function setActive(menuId) {
+        if (menuId == null) return;
+        var $menuItem = $linkContainer.find(".bh-menu-link-item[menuId='" + menuId + "']");
         //qiyu 2016-6-22 当activeChange事件返回false时，不要跳转tab页，需求人：吴涛
         // $menuItem.addClass("bh-active");
         var text = $menuItem.text();
@@ -60,85 +92,86 @@
 
         //qiyu 2016-6-22 当activeChange事件返回false时，不要跳转tab页，需求人：吴涛
         var flag = $linkContainer.triggerHandler("activeChange", [menuId, text, title, $menuItem]);
-        if(flag === false)return false;
+        if (flag === false) return false;
 
-        if(activeId){
-            $linkContainer.find(".bh-menu-link-item[menuId='"+activeId+"']").removeClass("bh-active");
-            $contentContainer.find(".bh-menu-content[id='"+activeId+"']").hide();
+        if (activeId) {
+            $linkContainer.find(".bh-menu-link-item[menuId='" + activeId + "']").removeClass("bh-active");
+            $contentContainer.find(".bh-menu-content[id='" + activeId + "']").hide();
         }
         activeId = menuId;
 
         $menuItem.addClass("bh-active");
 
-        $contentContainer.find(".bh-menu-content[id='"+activeId+"']").show();
+        $contentContainer.find(".bh-menu-content[id='" + activeId + "']").show();
         //qiyu 2016-6-22 当activeChange事件返回false时，不要跳转tab页，需求人：吴涛
         //给菜单项绑定激活项改变事件，返回菜单id,text和该节点
         // $linkContainer.trigger("activeChange", [menuId, text, title, $menuItem]);
     }
 
-    function getIdByIndex(_index){
+    function getIdByIndex(_index) {
         var id = null;
-        if(source){
+        if (source) {
             id = source[_index]["id"];
         }
         return id;
     }
-    function addEventListener(){
-        $linkContainer.on("click",".bh-menu-link-item",function(){
-            if($(this).hasClass("bh-disabled")) return false;
-            if(mode == "link"){
+
+    function addEventListener() {
+        $linkContainer.on("click", ".bh-menu-link-item", function() {
+            if ($(this).hasClass("bh-disabled")) return false;
+            if (mode == "link") {
                 var _url = $(this).attr("menu-data-url");
                 window.open(_url);
-            }else{
+            } else {
                 var menuId = $(this).attr("menuId");
                 setActive(menuId);
             }
         });
     }
-    function disableByIndex(_index){
+
+    function disableByIndex(_index) {
         var _id = getIdByIndex(_index);
-        $linkContainer.find(".bh-menu-link-item[menuId='"+_id+"']").addClass("bh-disabled");
+        $linkContainer.find(".bh-menu-link-item[menuId='" + _id + "']").addClass("bh-disabled");
     }
-    function enableByIndex(_index){
+
+    function enableByIndex(_index) {
         var _id = getIdByIndex(_index);
-        $linkContainer.find(".bh-menu-link-item[menuId='"+_id+"']").removeClass("bh-disabled");
+        $linkContainer.find(".bh-menu-link-item[menuId='" + _id + "']").removeClass("bh-disabled");
     }
-    function getLinkDom(_data){
+
+    function getLinkDom(_data) {
         var _html = '';
-        if(_data){
+        if (_data) {
             _html = '<div class="bh-menu-link">';
-            for(var i=0,len=_data.length;i<len;i++){
-                _html+=getLinkItemDom(_data[i]);
+            for (var i = 0, len = _data.length; i < len; i++) {
+                _html += getLinkItemDom(_data[i]);
             }
             _html += '</div>';
         }
         return _html;
     }
-    function getLinkItemDom(_data){
+
+    function getLinkItemDom(_data) {
         var _html = '';
-        if(_data){
-            if(mode == "link"){
-                _html = '<div class="bh-menu-link-item" menuId="'+_data.id+'" menu-data-url="'+_data.url+'" data-title="'+_data.title+'">';
-            }else{
-                _html = '<div class="bh-menu-link-item" menuId="'+_data.id+'" data-title="'+_data.title+'">';
+        if (_data) {
+            if (mode == "link") {
+                _html = '<div class="bh-menu-link-item" menuId="' + _data.id + '" menu-data-url="' + _data.url + '" data-title="' + _data.title + '">';
+            } else {
+                _html = '<div class="bh-menu-link-item" menuId="' + _data.id + '" data-title="' + _data.title + '">';
             }
             _html += _data.title;
             _html += '</div>';
         }
         return _html;
     }
-    /**
-     * 这里是关键
-     * 定义一个插件 plugin
-     */
-    $.fn.bhMenu = function (options, params) {
+    $.fn.bhMenu = function(options, params) {
         var instance;
         instance = this.data('bhMenu');
         /**
          * 判断插件是否已经实例化过，如果已经实例化了则直接返回该实例化对象
          */
         if (!instance) {
-            return this.each(function () {
+            return this.each(function() {
                 //将实例化后的插件缓存在dom结构里（内存里）
                 return $(this).data('bhMenu', new Plugin(this, options));
             });
@@ -155,12 +188,18 @@
     };
 
     /**
-     * 插件的默认值
+     * @memberof module:bhMenu
+     * @description 内置默认值
+     * @prop {object}  defaults
+     * @prop {Array}  defaults.source - 菜单项的数据
+     * @prop {string}  defaults.activeIndex - 当前 active 项的index值
+     * @prop {$}  defaults.contentContainer - 内容所在的容器
+     * @prop {string}  defaults.mode - 页面跳转还是tab页切换
      */
     $.fn.bhMenu.defaults = {
-        source:[], //菜单项的数据 id,title 如果是mode="link"需要传url
-        activeIndex:0, //默认选中第几项
-        contentContainer:$("body"), //内容所在的容器
-        mode:"tab" //支持页面跳转还是tab页切换 支持两个值link/tab
+        source: [], //菜单项的数据 id,title 如果是mode="link"需要传url
+        activeIndex: 0, //默认选中第几项
+        contentContainer: $("body"), //内容所在的容器
+        mode: "tab" //支持页面跳转还是tab页切换 支持两个值link/tab
     };
 })(jQuery);
